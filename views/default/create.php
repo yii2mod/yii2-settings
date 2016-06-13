debab70b948bb8a7324b52f7160a31e2dc077142
Igor Chepurnoy <igorzfort@gmail.com>
2016-06-13 11:54:23 +0300

    Update create.php

--- views/default/create.php
+++ views/default/create.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 
+/* @var $this \yii\web\View */
+/* @var $model \yii2mod\settings\models\SettingModel */
+
 $this->title = 'Create Setting';
 $this->params['breadcrumbs'][] = ['label' => 'Settings', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
