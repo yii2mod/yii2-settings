520b4bf1823bbad6d1b106575b6929fb942abbd0
Igor Chepurnoy <igorzfort@gmail.com>
2016-06-13 11:54:37 +0300

    Update update.php

--- views/default/update.php
+++ views/default/update.php
@@ -2,17 +2,19 @@
 
 use yii\helpers\Html;
 
+/* @var $this \yii\web\View */
+/* @var $model \yii2mod\settings\models\SettingModel */
+
 $this->title = 'Update Setting: ' . $model->section . '.' . $model->key;
 $this->params['breadcrumbs'][] = ['label' => 'Settings', 'url' => ['index']];
 $this->params['breadcrumbs'][] = 'Update';
-
 ?>
 <div class="setting-update">
 
     <h1><?php echo Html::encode($this->title) ?></h1>
 
     <?php echo $this->render('_form', [
-        'model' => $model,
+        'model' => $model
     ]);
     ?>
 
